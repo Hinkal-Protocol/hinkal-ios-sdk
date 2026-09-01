83bc6b23e9c2b0652f1507828858ce0411fbb433
sabaaa1 <sdeli23@freeuni.edu.ge>
2026-09-01 18:14:36 +0400

    Release iOS SDK 0.1.2
    Rebuild of the gomobile xcframework from current Hinkal-Protocol libs/go.
    Also repoint package URL at the renamed hinkal-ios-sdk repository.
    
    Breaking changes since 0.1.1:
    - storeUtxoInEnclave renamed to storeClaimableKeyInEnclave
    - removed MobileGetOdosQuoteJSON, MobileGetOneInchQuoteJSON,
      MobileGetUniswapQuoteJSON
    - removed MobileActionOdos, MobileActionOneInch, MobileActionUniswap
    
    Added:
    - getStuckShieldedBalance, getTotalBalances
    - MobileGetOKXQuoteJSON, MobileGetLifiBridgeQuoteJSON,
      MobileGetNearIntentsQuoteJSON, MobileGetSwapQuotesJSON
    - MobileActionOkx, MobileActionLifi, MobileActionEmporium,
      MobileActionTransact

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Hinkal",
-            url: "https://github.com/Hinkal-Protocol/hinkal-sdk/releases/download/0.1.1/Hinkal.xcframework.zip",
-            checksum: "9a986a6acf454ee73512aae3667377c1ae0622972ebbb6c841b49149121cb32a"
+            url: "https://github.com/Hinkal-Protocol/hinkal-ios-sdk/releases/download/0.1.2/Hinkal.xcframework.zip",
+            checksum: "2f7c0b825a607e6dc1284da4d32cfd4218c5bc08afb451a2fa82381518856a99"
         )
     ]
 )
